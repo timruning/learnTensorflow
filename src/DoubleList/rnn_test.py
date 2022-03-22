69463f32a5b11269e54a482132f843988c82147a
宋峰Feng Song <fengsong@clubfactory.com>
2022-03-22 16:57:57 +0800

    songfeng

--- src/DoubleList/rnn_test.py
+++ src/DoubleList/rnn_test.py
@@ -16,27 +16,27 @@ def check(x: list):
 
 if __name__ == '__main__':
     print(10)
-    x = tf.placeholder(shape=[None, 3], dtype=tf.int32)
+    x = tf.placeholder(shape=[None, 12], dtype=tf.int32)
     y = tf.placeholder(shape=[None], dtype=tf.float32)
     embedding = tf.Variable(tf.random_uniform((10, 8), -1, 1), name=f'embedding', trainable=False)
     layer = tf.Variable(tf.random_uniform([8, 1], -1, 1), name="layer", trainable=True)
     emb = tf.nn.embedding_lookup(embedding, x)
     cell = tf.keras.layers.SimpleRNNCell(8)
     emb2 = tf.keras.layers.LSTM(8)(emb)
-    # pred = tf.matmul(emb2, layer)
+    k = tf.matmul(emb2, layer)
     pred = tf.squeeze(tf.nn.sigmoid(tf.matmul(emb2, layer)))
-    print(y, tf.matmul(emb2, layer), pred)
     loss = tf.reduce_sum(tf.keras.losses.binary_crossentropy(y_true=y, y_pred=pred))
-    op = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+    op = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
         for i in range(1000000):
-            x1 = np.random.randint(10, size=[256, 3])
+            x1 = np.random.randint(10, size=[256, 12])
             y1 = [check(v) for v in x1]
             _loss, _emb2, _pred, _op = sess.run([loss, emb2, pred, op], feed_dict={
                 x: x1, y: y1
             })
             if i % 5000 == 1:
                 print(i, x1[0], y1[0], _pred[0])
+                print(_emb2[0])
                 print(_loss)
