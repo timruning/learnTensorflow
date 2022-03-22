f09c6cc2272e1152a9b658d6c2bb12698f064391
宋峰Feng Song <fengsong@clubfactory.com>
2022-03-22 19:11:24 +0800

    songfeng

--- src/DoubleList/rnn_test.py
+++ src/DoubleList/rnn_test.py
@@ -16,7 +16,7 @@ def check(x: list):
 
 if __name__ == '__main__':
     print(10)
-    x = tf.placeholder(shape=[None, 12], dtype=tf.int32)
+    x = tf.placeholder(shape=[None, 4], dtype=tf.int32)
     y = tf.placeholder(shape=[None], dtype=tf.float32)
     embedding = tf.Variable(tf.random_uniform((10, 8), -1, 1), name=f'embedding', trainable=False)
     layer = tf.Variable(tf.random_uniform([8, 1], -1, 1), name="layer", trainable=True)
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     with tf.Session() as sess:
         sess.run(init)
         for i in range(1000000):
-            x1 = np.random.randint(10, size=[256, 12])
+            x1 = np.random.randint(10, size=[256, 4])
             y1 = [check(v) for v in x1]
             _loss, _emb2, _pred, _op = sess.run([loss, emb2, pred, op], feed_dict={
                 x: x1, y: y1
